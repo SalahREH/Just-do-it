5f142491261fcbf7009b814a173367e06ca2eaa0
Bubbl3Waffl3 <salahlfd0@gmail.com>
2023-02-08 10:13:17 +0100

    update lists and tasks funcionality done

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,17 +2,17 @@
 import React, { useState, useEffect } from 'react'
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { useNavigate } from 'react-router';
+import { v4 as uuidv4 } from 'uuid';
+
 
 
 function HomePage() {
 
   let [lists, setLists] = useState([]);
   let [input, setInput] = useState("")
+  let [edit, setEdit] = useState({ id: null })
   let navigate = useNavigate()
-  let listObj = {
-    name: "Lista de la compra",
-    tasks: ["cebolla", "patatas", "detergente", "leche"]
-  };
+
 
 
   const handleChange = e => {
@@ -20,43 +20,87 @@ function HomePage() {
   }
 
   const handleKeyDown = e => {
+    if (!e.target.value || /^\s*$/.test(e.target.value)) {
+      return
+    }
+
     if (e.keyCode == 13) {
-      lists.push({ name: e.target.value, tasks: [] })
+      lists.push({ id: uuidv4(), name: e.target.value, tasks: [] })
       setLists(lists)
       localStorage.setItem("lists", JSON.stringify(lists))
       setInput('')
     }
   }
 
+  const removeList = id => {
+    let newArr = [...lists].filter(list => list.id != id)
+    setLists(newArr)
+  }
+
+  const handleEditChange = e => {
+    
+  }
+
+  const handleEditKeyDown = e => {
+    if (e.keyCode == 13) {
+      edit.name = e.target.value
+
+    //   // setLists(lists => lists.map(item => (item.id === newValue.id) ? newValue : item))
+      setEdit({ id: null })
+    }
+  }
+
+
+
+
   useEffect(() => {
     (function pushinarr() {
       let arr = []
       for (let index = 0; index < 3; index++) {
+        let listObj = {
+          id: uuidv4(),
+          name: "Lista de la compra",
+          tasks: [{id: uuidv4(), value: "cebolla"},{id: uuidv4(), value: "patatas"}, {id: uuidv4(), value: "detergente"}, {id: uuidv4(), value: "leche"}]
+        };
         arr.push(listObj)
       }
       setLists(arr)
     })()
   }, [])
 
+
   return (
     <div className='App-HomePage'>
-      <h2>Home</h2>
-      <div className='App-HomePage-lists'>
-        {lists.map((item, i) => (<div key={i} className='App-HomePage-lists-list'>
-          <p onClick={() => { navigate("/List", { state: item }) }}>{item.name}</p>
-          {/* <p className='App-HomePage-lists-list-name'></p> */}
-          {/* <img> urgent and three dots icons with react-icons </img> */}
-          <BsThreeDotsVertical />
-          <div className='App-HomePage-lists-list-underline'></div>
-        </div>))}
-        <div className='App-HomePage-lists-newList' /* onClick={navigate("")} */>
 
-          <input type="text" placeholder="New List..." value={input} onChange={handleChange} onKeyDown={handleKeyDown} />
-          {/* <img> urgent with react-icons </img> */}
-          <BsThreeDotsVertical />
-          <div className='App-HomePage-lists-list-underline'></div>
-        </div>
-      </div>
+      {edit.id ?
+        <>
+          <h2>Changing the name...</h2>
+          <div className='App-HomePage-lists'>
+            <div className='App-HomePage-lists-newList'>
+              <input type="text" placeholder="New name..." onKeyDown={handleEditKeyDown} />
+
+              <BsThreeDotsVertical />
+              <div className='App-HomePage-lists-list-underline'></div>
+            </div>
+          </div>
+        </>
+        :
+        <>
+          <h2>Home</h2>
+          <div className='App-HomePage-lists'>
+            {lists.map((item, i) => (<div key={i} className='App-HomePage-lists-list'>
+              <p onClick={() => { navigate("/List", { state: item }) }}>{item.name}</p>
+              {/* <img> urgent with react-icons </img> */}
+              <BsThreeDotsVertical onClick={() => { setEdit(item) }} />
+              <div className='App-HomePage-lists-list-underline'></div>
+            </div>))}
+            <div className='App-HomePage-lists-newList'>
+              <input type="text" placeholder="New List..." value={input} onChange={handleChange} onKeyDown={handleKeyDown} />
+              <BsThreeDotsVertical />
+              <div className='App-HomePage-lists-list-underline'></div>
+            </div>
+          </div>
+        </>}
     </div>
   )
 }
--- src/components/ListPage.jsx
+++ src/components/ListPage.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import { BsThreeDotsVertical } from "react-icons/bs";
-
+import { v4 as uuidv4 } from 'uuid';
 import { useLocation } from 'react-router-dom';
 
 function ListPage() {
     const list = useLocation().state
     let [input, setInput] = useState("")
     let [tasks, setTasks] = useState(list.tasks)
+    let [edit, setEdit] = useState({ id: null, value: '' })
 
     const handleChange = e => {
         setInput(e.target.value)
@@ -20,25 +21,50 @@ function ListPage() {
         }
     }
 
+    const handleEditChange = e => {
+    }
+
+    const handleEditKeyDown = e => {      
+        if (e.keyCode == 13) {
+            // setTasks(lists => lists.map((item, id) => (item == newValue.value) ? newValue.value : item))
+            edit.value = e.target.value
+            setEdit({ id: null, value: '' })
+        }
+    }
+
     return (
         <div className='App-ListPage'>
-            <h2>{list.name}</h2>
-            <div className='App-HomePage-lists'>
-                {tasks.map((item, i) => (<div key={i} className='App-HomePage-lists-list'>
-                    <p>{item}</p>
-                    {/* <p className='App-HomePage-lists-list-name'></p> */}
-                    {/* <img> urgent and three dots icons with react-icons </img> */}
-                    <BsThreeDotsVertical />
-                    <div className='App-HomePage-lists-list-underline'></div>
-                </div>))}
-                <div className='App-HomePage-lists-newList' /* onClick={navigate("")} */>
+            {edit.id ?
+                <>
+                    <h2>Changing the name...</h2>
+                    <div className='App-HomePage-lists'>
+                        <div className='App-HomePage-lists-newList'>
+                            <input type="text" placeholder="New name..." onChange={handleEditChange} onKeyDown={handleEditKeyDown} />
 
-                    <input type="text" placeholder="New List..." value={input} onChange={handleChange} onKeyDown={handleKeyDown} />
-                    {/* <img> urgent with react-icons </img> */}
-                    <BsThreeDotsVertical />
-                    <div className='App-HomePage-lists-list-underline'></div>
-                </div>
-            </div>
+                            <BsThreeDotsVertical />
+                            <div className='App-HomePage-lists-list-underline'></div>
+                        </div>
+                    </div>
+                </>
+                :
+                <>
+                    <h2>{list.name}</h2>
+                    <div className='App-ListPage-lists'>
+                        {tasks.map((item, i) => (<div key={i} className='App-ListPage-lists-list'>
+                            <p>{item.value}</p>
+                            {/* <img> urgent  with react-icons </img> */}
+                            <BsThreeDotsVertical onClick={() => { setEdit(item) }} />
+                            <div className='App-ListPage-lists-list-underline'></div>
+                        </div>))}
+                        <div className='App-ListPage-lists-newList' /* onClick={navigate("")} */>
+
+                            <input type="text" placeholder="New List..." value={input} onChange={handleChange} onKeyDown={handleKeyDown} />
+                            <BsThreeDotsVertical />
+                            <div className='App-ListPage-lists-list-underline'></div>
+                        </div>
+                    </div>
+                </>
+            }
         </div>
     )
 }
